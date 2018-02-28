032efca4d92bf02812108e820ef06b5373d3b931
Johann Stig Gravenhorst R. <jsgravenhorst@gmail.com>
2018-02-27 19:13:22 -0500

    Implementacion metodos HistoriaClinicaForma
    Se implemento los metodos constructor, getter y setter de
    HistoriaClinicaForma

--- softipac/src/main/java/com/olonte/softipac/modelo/HistoriaClinicaForma.java
+++ softipac/src/main/java/com/olonte/softipac/modelo/HistoriaClinicaForma.java
@@ -10,4 +10,40 @@ public class HistoriaClinicaForma {
 	
 	private Usuario madre;
 
+	public HistoriaClinicaForma() {
+	}
+
+	public HistoriaClinica getHistoriaClinica() {
+		return historiaClinica;
+	}
+
+	public void setHistoriaClinica(HistoriaClinica historiaClinica) {
+		this.historiaClinica = historiaClinica;
+	}
+
+	public Usuario getPaciente() {
+		return paciente;
+	}
+
+	public void setPaciente(Usuario paciente) {
+		this.paciente = paciente;
+	}
+
+	public Usuario getPadre() {
+		return padre;
+	}
+
+	public void setPadre(Usuario padre) {
+		this.padre = padre;
+	}
+
+	public Usuario getMadre() {
+		return madre;
+	}
+
+	public void setMadre(Usuario madre) {
+		this.madre = madre;
+	}
+
+
 }
